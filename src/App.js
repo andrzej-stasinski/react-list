b314631f0b0d5f7870545e9d7f5810e94250b5e9
Andrzej Stasinski <andis_as@wp.pl>
2021-01-05 22:48:11 +0100

    input & button add to state task

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 
 
 class ToDoList extends Component {
+  state = {
+    Draft: '',
+    tasks: this.props.tasks
+  }
+  updateDraft = (e) => {
+    this.setState({ Draft: e.target.value });
+  }
+
+  addToDo = () => {
+    this.setState({ tasks: this.state.tasks.concat(this.state.Draft), Draft: '' });
+  }
 
   render() { 
+    const {tasks, Draft} = this.state
     return (
       <div>
         <h1>{this.props.title}</h1>
-        {this.props.tasks.map(task => <div key={task}><p>{task}</p></div>)}
-        <input />
+        {tasks.map(task => <div key={task}><p>{task}</p></div>)}
+        <input type='text' onChange={this.updateDraft} value={Draft} />
+        <button onClick={this.addToDo}>Add</button>
       </div>
     );
   }
